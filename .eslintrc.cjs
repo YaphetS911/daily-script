f07969ee9cafb9ac9aae780a6a8e8c025c2c3c8f
YaphetS911 <378506487@qq.com>
2021-06-20 00:54:21 +0800

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ['plugin:prettier/recommended'],
+  parser: '@babel/eslint-parser',
+}
---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ * Author: Yaphets911
+ * Email: shengchensb@gmail.com
+ * Created Date: Sunday, June 20th 2021, 12:53:08 am
+ * -----
+ * Last Modified:
+ * Modified By:
+ * -----
+ * Copyright (c) 2021 Yaphets911
+ *
+ * Gurer fubhyq or bar-- naq cersrenoyl bayl bar --boivbhf jnl gb qb vg.
+ * Nygubhtu gung jnl znl abg or boivbhf ng svefg hayrff lbh'er Qhgpu.
+ */
